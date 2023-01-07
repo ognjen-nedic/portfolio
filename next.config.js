3db76a4574bbad2ed0ea793b32e337ff31094e43
ognjen-nedic <ognjen.n94@gmail.com>
2023-01-07 19:22:32 +0100

    open graph etc

--- next.config.js
+++ next.config.js
@@ -4,4 +4,30 @@ const nextConfig = {
   swcMinify: true,
 }
 
+const withAntdLess = require('next-plugin-antd-less');
+
+module.exports = withAntdLess({
+  // modifyVars: { '@primary-color': '#04f' }, // optional
+  lessVarsFilePath: './src/styles/variables.less', // optional
+  lessVarsFilePathAppendToEndOfContent: false, // optional
+  // optional https://github.com/webpack-contrib/css-loader#object
+  cssLoaderOptions: {
+    // ...
+    mode: "local"
+    , localIdentName: "[hash:base64:8]"
+    , exportLocalsConvention: "camelCase"
+    , exportOnlyLocals: false,
+    // ...
+    getLocalIdent: (context, localIdentName, localName, options) => {
+      return "whatever_random_class_name";
+    },
+  },
+
+  // Other Config Here...
+
+  webpack(config) {
+    return config;
+  },
+});
+
 module.exports = nextConfig
